652a8c5242c1bbfc586a116e981281036240b249
Yucky Hito <yuckychong@gmail.com>
2023-08-07 11:32:40 +0800

    Clean code for entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { FluentProvider, webLightTheme, webDarkTheme } from '@fluentui/react-components'
+import type { Theme } from '@fluentui/react-components'
+import ApplyToBody from './ApplyToBody'
+import App from './App'
 import './index.css'
 import 'modern-normalize'
-import type { Theme } from '@fluentui/react-components'
-import { FluentProvider, webLightTheme, webDarkTheme, useThemeClassName } from '@fluentui/react-components'
 
 const appTheme: Theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? webDarkTheme : webLightTheme
 
-function ApplyToBody() {
-  const classes = useThemeClassName();
-
-  React.useEffect(() => {
-    const classList = classes.split(" ");
-    document.body.classList.add(...classList);
-
-    return () => document.body.classList.remove(...classList);
-  }, [classes]);
-
-  return null;
-}
-
-ReactDOM.createRoot(document.querySelector('body')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root') ?? document.createElement('div')).render(
+  <StrictMode>
     <FluentProvider theme={appTheme}>
       <ApplyToBody />
       <App />
     </FluentProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
