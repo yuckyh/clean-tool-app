6e39618ca82328e558266ec96409c612c07b30de
Yucky Hito <yuckychong@gmail.com>
2023-08-11 18:09:35 +0800

    Use the correct strict mode configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { StrictMode } from 'react'
 
 const rootDOM = document.getElementById('root') ?? document.createElement('div')
 const root = createRoot(rootDOM)
-root.render(<App />)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
