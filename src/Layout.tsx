b1c0f9fbc12b871f9dfdffbded7a310955de6c29
Yucky Hito <yuckychong@gmail.com>
2023-08-11 18:09:08 +0800

    Add automatic route nav hydration

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,18 +1,47 @@
-import { TabList } from '@fluentui/react-components'
-import { Link, Outlet } from 'react-router-dom'
+import { Tab, TabList } from '@fluentui/react-components'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { router } from '@/router'
+import { useEffect, useState } from 'react'
 
 const Layout = () => {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+  const [routes, setRoutes] = useState<string[]>([])
+
+  useEffect(() => {
+    const rootRoute = router.routes[0]!
+    const navRoutes = rootRoute.children!
+    setRoutes(
+      navRoutes
+        .filter((route) => route.path !== '/' && route.path !== '*')
+        .map((route) => route.path!.split('/').join('')),
+    )
+  }, [])
+
   return (
     <>
-      <aside>
-        <TabList>
-          <Link to="/">Test</Link>
+      <header>
+        <TabList
+          className="w-full"
+          selectedValue={pathname}
+          onTabSelect={(_event, data) => {
+            if (!data?.value) return null
+            navigate(data!.value)
+            return data!.value
+          }}>
+          {routes.map((route) => (
+            <Tab
+              key={route}
+              value={`/${route}`}
+              style={{ textTransform: 'capitalize' }}>
+              {route}
+            </Tab>
+          ))}
         </TabList>
-      </aside>
-      <main className="grid-cols-3">
+      </header>
+      <main className="container-lg mx-auto">
         <Outlet />
       </main>
-      <aside></aside>
     </>
   )
 }
