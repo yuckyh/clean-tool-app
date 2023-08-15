2f47e8ca921fe6d0ce145f741f6c477046f3cfba
Yucky Hito <yuckychong@gmail.com>
2023-08-15 16:30:38 +0800

    Remove Tailwind andd add React Helmet

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss'
-import containerQueries from '@tailwindcss/container-queries'
-
-export default {
-  content: ["./src/**/*.{ts,tsx}"],
-  theme: {},
-  plugins: [containerQueries],
-} satisfies Config
-
