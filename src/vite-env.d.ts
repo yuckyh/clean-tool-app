b7e316d080077ed718e32bfac8b5852d741bc6c0
Yucky Hito <yuckychong@gmail.com>
2023-08-16 17:05:38 +0800

    Fix type declarations, use helmet, remove tailwind

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,13 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/info" />
 /// <reference types="vite-plugin-pwa/react" />
 
-declare module 'virtual:pwa-register/react' {
-  import type { Dispatch, SetStateAction } from 'react'
+declare module 'virtual:pwa-register' {
   import type { RegisterSWOptions } from 'vite-plugin-pwa/types'
 
   export type { RegisterSWOptions }
 
-  export function useRegisterSW(options?: RegisterSWOptions): {
-    needRefresh: [boolean, Dispatch<SetStateAction<boolean>>]
-    offlineReady: [boolean, Dispatch<SetStateAction<boolean>>]
-    updateServiceWorker: (reloadPage?: boolean) => Promise<void>
-  }
+  export function registerSW(
+    options?: RegisterSWOptions,
+  ): (reloadPage?: boolean) => Promise<void>
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import { VitePWA } from 'vite-plugin-pwa'
+import lightningcss from 'vite-plugin-lightningcss'
 import { resolve } from 'path'
 
 import { webManifest } from './package.json'
 
+const resolveDir = (dir: TemplateStringsArray) => resolve(__dirname, ...dir)
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
+  const isDev = mode === 'development'
 
   return {
     define: {
@@ -20,15 +23,21 @@ export default defineConfig(({ mode }) => {
         injectRegister: 'auto',
         devOptions: {
           enabled: true,
+          // navigateFallbackAllowlist: [/^\/(?!api\/)/],
         },
         workbox: {
-          cleanupOutdatedCaches: false,
-          sourcemap: !!env.TAURI_DEBUG,
+          sourcemap: isDev,
           globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         },
         manifest: webManifest,
+        srcDir: 'src',
+        filename: 'sw.ts',
       }),
-      basicSsl(),
+      lightningcss({
+        minify: !isDev,
+        sourceMap: isDev,
+        browserslist: "last 2 versions, not dead, >0.2%, ie 11"
+      }),
     ],
     // prevent vite from obscuring rust errors
     clearScreen: false,
@@ -36,12 +45,10 @@ export default defineConfig(({ mode }) => {
     server: {
       strictPort: true,
       cors: false,
-      https: true,
     },
     preview: {
       strictPort: true,
       cors: false,
-      https: true,
     },
     esbuild: {
       jsxInject: `import React from 'react'`,
@@ -54,17 +61,14 @@ export default defineConfig(({ mode }) => {
       // Tauri uses Chromium on Windows and WebKit on macOS and Linux
       target: env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
       // don't minify for debug builds
-      minify: env.TAURI_DEBUG ? false : 'esbuild',
+      minify: isDev ? false : 'esbuild',
       // produce sourcemaps for debug builds
-      sourcemap: !!env.TAURI_DEBUG,
-      cssMinify: env.TAURI_DEBUG ? false : 'lightningcss',
+      sourcemap: isDev,
+      cssMinify: isDev ? false : 'lightningcss',
     },
-    // css: {
-    //   transformer: 'lightningcss',
-    // },
     resolve: {
       alias: {
-        '@': resolve(__dirname, 'src'),
+        '@': resolveDir`src`,
       },
     },
   }
