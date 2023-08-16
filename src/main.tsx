0d70aff3440fafc0a3eb5858c73f35ca05c04587
Yucky Hito <yuckychong@gmail.com>
2023-08-16 17:08:05 +0800

    Add service worker registration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import App from '@/App'
 import { StrictMode } from 'react'
+import { registerSW } from 'virtual:pwa-register'
+
+const intervalMS = 60 * 60 * 1000
+
+registerSW({
+  immediate: true,
+  onOfflineReady() {
+    console.log('offline ready')
+  },
+  onRegistered(r) {
+    r &&
+      setInterval(() => {
+        r.update()
+      }, intervalMS)
+  },
+})
 
 const rootDOM = document.getElementById('root') ?? document.createElement('div')
 const root = createRoot(rootDOM)
+
 root.render(
   <StrictMode>
     <App />
