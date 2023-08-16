e51ed45fb2ecd4c371673089d5f14d39a0790c87
Yucky Hito <yuckychong@gmail.com>
2023-08-16 17:20:35 +0800

    Fix navigation animations

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -15,6 +15,7 @@ import {
 
 import { getRouteName } from '@/helpers'
 import { routes } from '@/router'
+import { useState } from 'react'
 
 type NavProps = TabListProps & {
   navRoutes: RouteObject[]
@@ -25,11 +26,18 @@ const Nav = ({ navRoutes, ...props }: NavProps) => {
   const location = useLocation()
   const currentRoute = matchRoutes(routes, location)!.pop()!.route!
   const title = getRouteName(currentRoute!)
+  const [resolvedPaths] = useState(
+    navRoutes.map(
+      (route) => resolvePath(route.path!, location.pathname).pathname,
+    ),
+  )
 
   const handleTabSelect: SelectTabEventHandler = (_event, data) => {
     navigate(data.value ?? '')
     return data.value ?? ''
   }
+
+  console.log(resolvePath)
 
   return (
     <>
@@ -40,13 +48,13 @@ const Nav = ({ navRoutes, ...props }: NavProps) => {
         selectedValue={location.pathname}
         onTabSelect={handleTabSelect}
         {...props}>
-        {navRoutes.map((route) => (
-          <Tab
-            key={route.id}
-            value={resolvePath(route.path!, location.pathname).pathname}>
-            {getRouteName(route)}
-          </Tab>
-        ))}
+        {navRoutes.map((route, i) => {
+          return (
+            <Tab key={route.id} value={resolvedPaths[i]}>
+              {getRouteName(route)}
+            </Tab>
+          )
+        })}
       </TabList>
     </>
   )
