7fd208f5bc8343e18ab5c5af3a6476b4071ecc9a
Yucky Hito <yuckychong@gmail.com>
2023-08-31 10:16:19 +0800

    Remove context and the need for it

--- src/contexts.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-import FileWorker from '@/workers/file?worker'
-
-export const FileWorkerContext = createContext<Worker>(new FileWorker())
-export const FileNameContext = createContext<string>('')
