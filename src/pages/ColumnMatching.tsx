ed64154ba8c99b8858ea0a45c14b8e41336b4126
Yucky Hito <yuckychong@gmail.com>
2023-09-04 16:47:55 +0800

    Remove action function export

--- src/pages/ColumnMatching.tsx
+++ src/pages/ColumnMatching.tsx
@@ -1,19 +1,18 @@
-import { ProgressState, progressManager } from '@/lib/ProgressManager'
-import { Form, type ActionFunction } from 'react-router-dom'
+import { Form } from 'react-router-dom'
 
 export const Component = () => {
+  // useEffect(() => {
+  //   void (async () => {
+  //     file && console.log(XLSX.read(await file.arrayBuffer()))
+  //   })()
+  // }, [file])
+
+  console.log()
   return (
     <Form>
       <h1>Column Matching</h1>
     </Form>
   )
-}
-
-// eslint-disable-next-line react-refresh/only-export-components
-export const action: ActionFunction = (args) => {
-  console.log(args)
-  progressManager.state = ProgressState.UPLOADED
-  return { status: 200 }
 }
 
 Component.displayName = 'ColumnMatching'
