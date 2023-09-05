96cd6f966ed372fa0898637209ba20a615b0fc9b
Yucky Hito <yuckychong@gmail.com>
2023-09-06 01:15:58 +0800

    Remove unneeded module declaration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,12 +2,3 @@
 /// <reference types="vite-plugin-pwa/info" />
 /// <reference types="vite-plugin-pwa/react" />
 
-// declare module 'virtual:pwa-register' {
-//   import type { RegisterSWOptions } from 'vite-plugin-pwa/types'
-
-//   export type { RegisterSWOptions }
-
-//   export function registerSW(
-//     options?: RegisterSWOptions,
-//   ): (reloadPage?: boolean) => Promise<void>
-// }
