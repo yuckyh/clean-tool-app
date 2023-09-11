36e65cf96ff06f4db5d50f2bf2a50c542f39741b
Yucky Hito <yuckychong@gmail.com>
2023-09-11 10:17:22 +0800

    Create an empty preview data grid

---
+++ src/components/PreviewDataGrid.tsx
@@ -0,0 +1,9 @@
+import SimpleDataGrid from './SimpleDataGrid'
+
+const PreviewDataGrid = () => {
+  return (
+    <SimpleDataGrid items={[]} dataGridProps={{ items: [], columns: [] }} />
+  )
+}
+
+export default PreviewDataGrid
