b60289bbe769c882803adf2699fae0d0abe4c5f2
Yucky Hito <yuckychong@gmail.com>
2023-09-11 09:42:06 +0800

    Use plotly's type declaration for smaller bundle

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/info" />
 /// <reference types="vite-plugin-pwa/react" />
+
+declare module 'plotly.js-cartesian-dist' {
+  import Plotly from 'plotly.js'
+  export default Plotly
+}
