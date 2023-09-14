a25b14eca8fe962bbd51612e65651158d4bee818
Yucky Hito <yuckychong@gmail.com>
2023-09-14 16:44:14 +0800

    Move closures out

--- src/lib/plotly.ts
+++ src/lib/plotly.ts
@@ -8,29 +8,26 @@ export const tokenToColor = (token: ColorToken) =>
     token.substring(4, token.length - 1),
   )
 
+const hexToRgb = (hexColor: string): number[] => {
+  return [
+    parseInt(hexColor.slice(1, 3), 16),
+    parseInt(hexColor.slice(3, 5), 16),
+    parseInt(hexColor.slice(5, 7), 16),
+  ]
+}
+const rgbToHex = (rgbColor: number[]): string => {
+  return '#' + rgbColor.map((c) => c.toString(16).padStart(2, '0')).join('')
+}
+
 export const fluentColorScale = (
   color1Token: ColorToken,
   color2Token: ColorToken,
   n: number,
-): Plotly.ColorScale => {
-  const hexToRgb = (hexColor: string): number[] => {
-    return [
-      parseInt(hexColor.slice(1, 3), 16),
-      parseInt(hexColor.slice(3, 5), 16),
-      parseInt(hexColor.slice(5, 7), 16),
-    ]
-  }
-  const rgbToHex = (rgbColor: number[]): string => {
-    return '#' + rgbColor.map((c) => c.toString(16).padStart(2, '0')).join('')
-  }
-  const rgb1 = hexToRgb(tokenToColor(color1Token))
-  const rgb2 = hexToRgb(tokenToColor(color2Token))
-
-  return Array.from({ length: n }, (_, i): [number, string] => {
+): Plotly.ColorScale =>
+  Array.from({ length: n }, (_, i): [number, string] => {
     const t = i / (n - 1)
-    const interpolatedRgb = rgb1.map((c1, j) =>
-      Math.round(c1 + t * ((rgb2[j] ?? 0) - c1)),
+    const interpolatedRgb = hexToRgb(tokenToColor(color1Token)).map((c1, j) =>
+      Math.round(c1 + t * ((hexToRgb(tokenToColor(color2Token))[j] ?? 0) - c1)),
     )
     return [t, rgbToHex(interpolatedRgb)]
   })
-}
