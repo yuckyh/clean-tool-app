060aad721ef111b29015bcb67c9d4b0bc381f2b9
Yucky Hito <yuckychong@gmail.com>
2023-09-14 16:06:28 +0800

    Make simple data grid more reusable

--- src/components/SimpleDataGrid.tsx
+++ src/components/SimpleDataGrid.tsx
@@ -1,6 +1,9 @@
 import type {
+  DataGridCellFocusMode,
   DataGridProps,
   TableColumnDefinition,
+  TableColumnId,
+  TableRowData,
 } from '@fluentui/react-components'
 import {
   DataGrid,
@@ -9,33 +12,72 @@ import {
   DataGridHeaderCell,
   DataGridBody,
   DataGridCell,
+  makeStyles,
 } from '@fluentui/react-components'
+import { useCallback } from 'react'
 
-interface SimpleDataGridProps<T> {
+interface SimpleDataGridProps<T, K>
+  extends Partial<Omit<DataGridProps, 'items' | 'columns'>> {
   items: T[]
-  dataGridProps: DataGridProps
+  columns: TableColumnDefinition<K>[]
+  cellFocusMode: (columnId: TableColumnId) => DataGridCellFocusMode
 }
 
-const SimpleDataGrid: <T>(props: SimpleDataGridProps<T>) => JSX.Element = ({
-  items,
-  dataGridProps,
-}) => (
-  <DataGrid {...dataGridProps}>
-    <DataGridHeader>
-      <DataGridRow>
-        {({ renderHeaderCell }) => (
-          <DataGridHeaderCell>{renderHeaderCell()}</DataGridHeaderCell>
-        )}
-      </DataGridRow>
-    </DataGridHeader>
-    <DataGridBody<(typeof items)[0]>>
-      {({ item, rowId }) => (
-        <DataGridRow<(typeof items)[0]> key={rowId}>
-          {({ renderCell }) => <DataGridCell>{renderCell(item)}</DataGridCell>}
+const useClasses = makeStyles({
+  grid: {
+    width: '100%',
+    overflowX: 'auto',
+  },
+  container: {
+    width: '100%',
+    minWidth: 'fit-content',
+  },
+  cell: {
+    minWidth: '160px',
+  },
+})
+
+const SimpleDataGrid: <T, K>(
+  props: SimpleDataGridProps<T, K>,
+) => JSX.Element = (props) => {
+  const { items, cellFocusMode } = props
+  const classes = useClasses()
+
+  const renderHeaderCell: <K>(def: TableColumnDefinition<K>) => JSX.Element =
+    useCallback(
+      ({ renderHeaderCell }) => (
+        <DataGridHeaderCell className={classes.cell}>
+          {renderHeaderCell()}
+        </DataGridHeaderCell>
+      ),
+      [classes.cell],
+    )
+
+  const renderCell: (def: TableRowData<(typeof items)[1]>) => JSX.Element =
+    useCallback(
+      ({ item, rowId }) => (
+        <DataGridRow<(typeof items)[1]> key={rowId}>
+          {({ renderCell, columnId }) => (
+            <DataGridCell
+              className={classes.cell}
+              focusMode={cellFocusMode(columnId)}>
+              {renderCell(item)}
+            </DataGridCell>
+          )}
         </DataGridRow>
-      )}
-    </DataGridBody>
-  </DataGrid>
-)
+      ),
+      [cellFocusMode, classes.cell],
+    )
+  return (
+    <DataGrid {...props} className={classes.grid}>
+      <DataGridHeader className={classes.container}>
+        <DataGridRow>{renderHeaderCell}</DataGridRow>
+      </DataGridHeader>
+      <DataGridBody<(typeof items)[1]> className={classes.container}>
+        {renderCell}
+      </DataGridBody>
+    </DataGrid>
+  )
+}
 
 export default SimpleDataGrid
