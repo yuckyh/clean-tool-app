575fd668822a48d7983dce6fb233d84f6f205420
Yucky Hito <yuckychong@gmail.com>
2023-09-19 16:12:12 +0800

    Add column change preview

--- src/pages/ColumnMatching.tsx
+++ src/pages/ColumnMatching.tsx
@@ -1,10 +1,15 @@
 import ColumnsDataGrid from '@/components/ColumnsDataGrid'
+import SimpleDataGrid from '@/components/SimpleDataGrid'
+import { useAppSelector } from '@/lib/hooks'
 import {
   Title1,
+  createTableColumn,
   makeStyles,
   shorthands,
   tokens,
 } from '@fluentui/react-components'
+import { useMemo } from 'react'
+import { utils } from 'xlsx'
 
 const useClasses = makeStyles({
   root: {
@@ -15,14 +20,44 @@ const useClasses = makeStyles({
   },
 })
 
+type CellItem = Record<Column, boolean | number | string>
+type Column = number | string
+
 export const Component = () => {
   const classes = useClasses()
+
+  const { sheet } = useAppSelector(({ sheet }) => sheet)
+  const { formattedColumns, originalColumns } = useAppSelector(
+    ({ columns }) => columns,
+  )
+
+  const data = useMemo(
+    () => (sheet ? utils.sheet_to_json(sheet) : [[]]),
+    [sheet],
+  )
+
+  const columnItems = useMemo(
+    () =>
+      formattedColumns.map((column, i) =>
+        createTableColumn<CellItem>({
+          columnId: column,
+          renderCell: (item) => <>{item[originalColumns[i] ?? '']}</>,
+          renderHeaderCell: () => <>{column}</>,
+        }),
+      ),
+    [formattedColumns, originalColumns],
+  )
 
   return (
     <section className={classes.root}>
       <Title1>Column Matching</Title1>
 
       <ColumnsDataGrid />
+      <SimpleDataGrid
+        cellFocusMode={() => 'none'}
+        columns={columnItems}
+        items={data.slice(1, 6)}
+      />
     </section>
   )
 }
