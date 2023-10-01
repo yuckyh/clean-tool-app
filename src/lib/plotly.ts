a064438ad6bcc4ac4c1c42c72b56820972c10f40
Yucky Hito <yuckychong@gmail.com>
2023-10-02 00:16:16 +0800

    Use more declarative code leveraging monads

--- src/lib/plotly.ts
+++ src/lib/plotly.ts
@@ -1,9 +1,10 @@
 import type { ColorTokens } from '@fluentui/react-components'
 
 import { transpose } from './array'
-import { curry, just, list } from './utils'
+import { just, list } from './utils'
 
 type ColorToken = Property<ColorTokens>
+type Rgb = readonly [number, number, number]
 
 export const tokenToHex = (token: ColorToken) =>
   getComputedStyle(document.body).getPropertyValue(
@@ -12,30 +13,38 @@ export const tokenToHex = (token: ColorToken) =>
 
 const parseHex = (hexString: string) => parseInt(hexString, 16)
 
-const hexToRgb = (hexColor: string) =>
-  list([1, 3, 5] as const)((x) => hexColor.substring(x, x + 2))(parseHex)()
+const splitHexString = (hexString: string) =>
+  list([1, 3, 5] as const)((x) => hexString.substring(x, x + 2))()
 
-const rgbToHex = (rgb: number[] | readonly number[]) =>
-  '#' + rgb.map((c) => c.toString(16).padStart(2, '0')).join('')
+const hexToRgb = (hexString: string) =>
+  just(hexString)(splitHexString).convert(list)(parseHex)() as Rgb
 
-const interpolate = (time: number, [start, end]: [number, number]) =>
-  start + time * (end - start)
+const numToHex = (x: number) => x.toString(16).padStart(2, '0')
 
-const tokensToInterpolatedHex = (tokens: readonly ColorToken[], t: number) =>
-  list(tokens)(tokenToHex)(hexToRgb)
-    .convert(just)(transpose)
-    .convert(list)((x) => x as [number, number])(curry(interpolate)(t))(
-      Math.round,
-    )
-    .convert(just)(rgbToHex)()
+const interpolate =
+  (time: number) =>
+  ([start, end]: readonly [number, number]) =>
+    Math.round(start + time * (end - start))
 
 const divideBy = (n: number) => (x: number) => x / n
+
+const timeToColorStep = (rgbDiffs: Transpose<[Rgb, Rgb]>) => (t: number) => [
+  t,
+  list(rgbDiffs)(interpolate(t))(numToHex).convert(just)((x) => x.join(''))(),
+]
 
 export const fluentColorScale = (
   color1Token: ColorToken,
   color2Token: ColorToken,
   n: number,
-) =>
-  just(n)(Array)(Array.from).convert(list)((_, i) => i)(divideBy(n - 1))(
-    (t) => [t, tokensToInterpolatedHex([color1Token, color2Token], t)],
+) => {
+  const rgbDiffs = list([color1Token, color2Token] as const)(tokenToHex)(
+    hexToRgb,
+  )
+    .convert(just)((x) => x as [Rgb, Rgb])(transpose)
+    .convert(list)()
+
+  return just(n)(Array)(Array.from).convert(list)((_, i) => i)(divideBy(n - 1))(
+    timeToColorStep(rgbDiffs),
   )() as Plotly.ColorScale
+}
