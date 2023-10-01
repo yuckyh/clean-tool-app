a1a02d843e86ee02b40bf1b106b180ba68a3c6fe
Yucky Hito <yuckychong@gmail.com>
2023-10-02 01:01:48 +0800

    Simplify variable user experience

--- src/pages/EDA/Variable.tsx
+++ src/pages/EDA/Variable.tsx
@@ -1,56 +1,34 @@
 import Plot from '@/components/Plot'
 import { codebook } from '@/data'
-import { getFormattedData } from '@/features/sheetSlice'
-import { useAppSelector } from '@/lib/hooks'
+import { fetchWorkbook, getFormattedData } from '@/features/sheetSlice'
+import {
+  useAppDispatch,
+  useAppSelector,
+  useAsyncEffect,
+  useLoadingTransition,
+} from '@/lib/hooks'
 import { slugToSnake } from '@/lib/string'
 import {
   Button,
   Card,
   CardFooter,
   CardHeader,
-  Dropdown,
   Field,
   Input,
-  Option,
   Title1,
   makeStyles,
   shorthands,
   tokens,
 } from '@fluentui/react-components'
-import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-type Measurement = Extract<Property<typeof measurements>, string>
+type VariableType = Extract<Property<typeof variableType>, string>
 
-type PlotType = Extract<Property<typeof plotTypes>, string>
+const variableType = ['numerical', 'categorical'] as const
 
-type PlotTypeMap = Record<Measurement, PlotType[]>
-
-const measurements = [
-  'categorical',
-  'continuous',
-  'discrete',
-  'ordinal',
-] as const
-
-// TODO: Hover show sno
+// TODO: data indexing for long data
+// TODO: Hover show index
 // TODO: data type detection
-// Continuous box plot
-// Categorical bar plot
-
-const plotTypes = ['bar', 'box', 'histogram', 'pie', 'violin'] as const
-
-const plotTypeMap: PlotTypeMap = {
-  categorical: ['bar', 'pie'],
-  continuous: ['box', 'histogram', 'violin'],
-  discrete: ['box', 'histogram', 'pie'],
-  ordinal: ['box', 'histogram', 'pie'],
-}
-
-interface VariableOptions {
-  measurement: Measurement
-  plotType: PlotType
-}
 
 const useClasses = makeStyles({
   actions: {
@@ -64,119 +42,81 @@ const useClasses = makeStyles({
     ...shorthands.margin(0, 'auto'),
     ...shorthands.padding(tokens.spacingHorizontalXXXL, '5%'),
   },
+  options: {
+    flexGrow: 1,
+    width: `${100 / 3}%`,
+  },
+  plot: {
+    flexShrink: 1,
+    width: `${(100 / 3) * 2}%`,
+  },
   root: {
-    display: 'grid',
-    gridAutoFlow: 'column',
-    width: '80%',
-    ...shorthands.margin(0, 'auto'),
-    ...shorthands.gap(0, tokens.spacingVerticalS),
+    display: 'flex',
+    width: '100%',
   },
 })
 
+const plotTypes: Record<VariableType, Plotly.PlotType> = {
+  categorical: 'bar',
+  numerical: 'box',
+}
+
 export const Component = () => {
   const params = useParams()
+  const dispatch = useAppDispatch()
+  const { fileName } = useAppSelector(({ sheet }) => sheet)
   const data = useAppSelector(getFormattedData)
 
   const classes = useClasses()
-  const [options, setOptions] = useState<VariableOptions>({
-    measurement: measurements[0],
-    plotType: plotTypeMap[measurements[0]][0] ?? 'box',
-  })
-  const [showPlot, setShowPlot] = useState(false)
-
   const variable = slugToSnake(params.variable ?? '')
+  const dataType = codebook.find(({ name }) => name === variable)?.type ?? ''
+  const type: VariableType = ['interval', 'whole_number'].includes(dataType)
+    ? 'numerical'
+    : 'categorical'
+  const [isLoading, setIsLoading] = useLoadingTransition()
+
   console.log(Object.keys(data[0] ?? {}))
-
+  console.log(dataType)
   console.log(variable)
+  console.log(Array.from(new Set(codebook.map(({ type }) => type))))
 
-  const series = data.map((row) => row[variable])
+  const series = data.map((row) => `${row[variable]}`)
 
-  // console.log(Array.from(new Set(codebook.map(({ type }) => type))))
-  // console.log(Array.from(new Set(codebook.map(({ unit }) => unit))))
-  console.log(series)
+  useAsyncEffect(async () => {
+    await dispatch(fetchWorkbook(fileName))
+    setIsLoading(false)
+  }, [dispatch, fileName])
 
   return (
-    <section className={classes.root}>
-      <section>
-        {showPlot ? (
-          <Plot data={[{ type: options.plotType, x: series }]} />
-        ) : (
-          'There is no plot yet'
-        )}
+    !isLoading && (
+      <section className={classes.root}>
+        <div className={classes.plot}>
+          <Plot data={[{ name: variable, type: plotTypes[type], x: series }]} />
+        </div>
+        <div className={classes.options}>
+          <Card className={classes.card} size="large">
+            <CardHeader header={<Title1>Plot Options</Title1>} />
+            {type !== 'categorical' && (
+              <>
+                <Field label="Minimum value">
+                  <Input appearance="filled-darker" type="number" />
+                </Field>
+                <Field label="Maximum value">
+                  <Input appearance="filled-darker" type="number" />
+                </Field>
+              </>
+            )}
+            <CardFooter
+              action={
+                <div className={classes.actions}>
+                  <Button appearance="primary">Plot</Button>
+                </div>
+              }
+            />
+          </Card>
+        </div>
       </section>
-      <section>
-        <Card className={classes.card} size="large">
-          <CardHeader header={<Title1>Plot Options</Title1>} />
-          <Field label="Variable type">
-            <Dropdown
-              appearance="filled-darker"
-              onOptionSelect={(e, { optionValue }) => {
-                setOptions((options) => ({
-                  measurement: optionValue as Measurement,
-                  plotType:
-                    (Object.keys(plotTypeMap).includes(options.measurement)
-                      ? options.plotType
-                      : plotTypeMap[optionValue as Measurement][0]) ?? 'box',
-                }))
-              }}
-              selectedOptions={[options.measurement]}
-              value={options.measurement}>
-              {measurements.map((measurement) => (
-                <Option key={measurement} value={measurement}>
-                  {measurement}
-                </Option>
-              ))}
-            </Dropdown>
-          </Field>
-          <Field label="Plot type">
-            <Dropdown
-              appearance="filled-darker"
-              onOptionSelect={(e, { optionValue }) => {
-                setOptions((options) => ({
-                  ...options,
-                  plotType: optionValue as PlotType,
-                }))
-              }}
-              selectedOptions={[options.plotType]}
-              value={options.plotType}>
-              {plotTypeMap[options.measurement].map((type) => (
-                <Option key={type} value={type}>
-                  {type}
-                </Option>
-              ))}
-            </Dropdown>
-          </Field>
-          {options.plotType === 'histogram' && (
-            <Field label="Number of bins">
-              <Input appearance="filled-darker" type="number" />
-            </Field>
-          )}
-          {options.measurement !== 'categorical' && (
-            <>
-              <Field label="Minimum value">
-                <Input appearance="filled-darker" type="number" />
-              </Field>
-              <Field label="Maximum value">
-                <Input appearance="filled-darker" type="number" />
-              </Field>
-            </>
-          )}
-          <CardFooter
-            action={
-              <div className={classes.actions}>
-                <Button
-                  appearance="primary"
-                  onClick={() => {
-                    setShowPlot(true)
-                  }}>
-                  Plot
-                </Button>
-              </div>
-            }
-          />
-        </Card>
-      </section>
-    </section>
+    )
   )
 }
 
